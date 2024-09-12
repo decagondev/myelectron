0305fd1a106fc3c7109605a72ff834be96b0358e
Tom Tarpey <tomtarpeydev@gmail.com>
2024-09-12 04:06:38 +0100

    added pixi

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,9 +1,42 @@
-function App(): JSX.Element {
-  return (
-    <div className="container">
-      lets get started
-    </div>
-  )
-}
+import "@pixi/unsafe-eval";
+import { useEffect } from 'react'
+import { Application, Text, TextStyle } from 'pixi.js'
+import  ReactDOM  from 'react-dom';
 
-export default App
+const pixiApp = new Application()
+
+
+const App = () => {
+  
+  useEffect(() => {
+    const myText = new Text("Hello Bob",
+        new TextStyle({
+          align: 'center',
+          fill: '0xffffff',
+          fontSize: 50,
+          letterSpacing: 20,
+          dropShadow: true,
+          dropShadowColor: '#E72264',
+          dropShadowDistance: 6,
+        })
+      );
+    pixiApp.stage.addChild(myText)
+    const bob = ReactDOM.findDOMNode(document.getElementById('bob'))
+    bob?.appendChild(pixiApp.renderer.view as any)
+    return () => {
+      bob?.removeChild(pixiApp.renderer.view as any)
+    }
+
+
+  }, [pixiApp]);
+
+  return (
+    <>
+      <div id="bob">
+        
+      </div>
+    </>
+  );
+};
+
+export default App;
